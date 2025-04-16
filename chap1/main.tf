eab469dae30270ad5b3fe315e7c053eeb9288842
Jonathan <atanajonathan5@gmail.com>
2025-04-16 14:47:19 +0100

    Update main.tf: create dynamic 'helloworld' config

--- chap1/main.tf
+++ chap1/main.tf
@@ -3,9 +3,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
+// Data sources
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"] # Amazon's official AMIs
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
 // The AWS instance resource
 resource "aws_instance" "hello-world" {
-  ami           = "ami-02f624c08a83ca16f" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = data.aws_ami.amazon-linux-2.id # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
 
   tags = {
@@ -14,7 +37,7 @@ resource "aws_instance" "hello-world" {
 }
 
 // Outputs
-output "Amazon-Linux-2-AMI" {
-  description = "The Image ID (AMI) of the Amazon Linux 2 instance"
-  value       = "ami-02f624c08a83ca16f"
+output "Amazon-Linux-2-Latest-AMI" {
+  description = "The Image ID (AMI) of the Amazon Linux 2's latest instance"
+  value       = data.aws_ami.amazon-linux-2.id
 }
