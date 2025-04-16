8412c55e67467606e67d2cb8f7038df88eab719c
Jonathan <atanajonathan5@gmail.com>
2025-04-16 22:01:55 +0100

    Update madlibs.tf: shuffle all lists in object variable (in-action/chap3)

--- chap3/madlibs.tf
+++ chap3/madlibs.tf
@@ -1,3 +1,14 @@
+// Terraform block
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+// Object for word pool
 variable "words" {
   description = "A word pool to use for Mad Libs"
   type = object({
@@ -12,4 +23,25 @@ variable "words" {
     condition = length(var.words["nouns"]) >= 20
     error_message = "At least 20 nouns are required."
   }
+}
+
+// Shuffle all lists using the random resource
+resource "random_shuffle" "random_nouns" {
+  input = var.words["nouns"]
+}
+
+resource "random_shuffle" "random_verbs" {
+  input = var.words["verbs"]
+}
+
+resource "random_shuffle" "random_adverbs" {
+  input = var.words["adverbs"]
+}
+
+resource "random_shuffle" "random_adjectives" {
+  input = var.words["adjectives"]
+}
+
+resource "random_shuffle" "random_numbers" {
+  input = var.words["numbers"]
 }
