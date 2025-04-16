206b1106cbfb47611b0722e01cb8a16b8e3c001a
Jonathan <atanajonathan5@gmail.com>
2025-04-16 19:23:14 +0100

    Use the terraform local resource (in-action/chap2)

---
+++ chap2/main.tf
@@ -0,0 +1,23 @@
+// Terraform setting block
+terraform {
+  required_providers {
+    local = {
+        source = "hashicorp/local"
+        version = "~> 2.1"
+    }
+  }
+}
+
+// Resource block
+resource "local_file" "litterature" {
+  filename = "art_of_war.txt"
+  content = <<-EOF
+  Sun Tzu said: The art of war is of vital importance to the State.
+
+  It is a matter of life and death, a road either to safety or to ruin. Hence it is a subject of inquiry which can on no account be neglected.
+
+  The art of war, then, is governed by five constant factors, to be taken into account in one's deliberations, when seeking to determine the conditions obtaining in the field.
+
+  These are: (1) The Moral Law; (2) Heaven; (3) Earth; (4) The Commander; (5) Method and discipline
+  EOF
+}
